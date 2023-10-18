4c5d8046f7f3f21cb9530c68258510cf58589583
Samuel-ALX23 <ayitey.samuel@gmail.com>
2023-10-18 14:27:22 +0000

    environ

---
+++ sy_env.c
@@ -0,0 +1,29 @@
+#include "shell.h"
+
+/**
+ * my_getenv - Retrieve the value of an environment variable.
+ * @varname: The name of the environment variable.
+ *
+ * Return: Null always (success)
+ */
+
+char *my_getenv(const char *varname)
+{
+	extern char **environ;
+	size_t varname_len = my_strlen(varname);
+
+	if (varname == NULL || environ == NULL)
+	{
+		return (NULL);
+	}
+
+	for (char **env = environ; *env != NULL; env++)
+	{
+		if (strncmp(varname, *env, varname_len) == 0 && (*env)[varname_len] == '=')
+		{
+			return (&((*env)[varname_len + 1]));
+		}
+	}
+
+	return (NULL);
+}
